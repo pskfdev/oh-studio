cc3608cc7252a322a41355d1b481ea27d146d061
Pongsakan-fluk <pongsakan.obr@futurelifestyle.co.th>
2025-03-30 13:56:47 +0700

    :recycle: Refactor code router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
-import AppRoutes from "./routes/AppRoutes";
-import "boxicons/css/boxicons.min.css";
+import { BrowserRouter, Route, Routes } from "react-router";
+//Layout
+import Layout from "./layouts/Layout";
+//Pages
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import Contact from "./pages/Contact";
+import Notfound from "./pages/Notfound";
+
 
 function App() {
-  return <AppRoutes />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/contact" element={<Contact />} />
+        </Route>
+
+        <Route path="*" element={<Notfound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
+import "boxicons/css/boxicons.min.css";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
