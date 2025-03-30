88251aa87f1df69c793c8fbc00e90c9b9c1eb8e8
Pongsakan-fluk <pongsakan.obr@futurelifestyle.co.th>
2025-03-30 23:31:27 +0700

    :bug: Fix bug button finish

--- src/components/form/Question.tsx
+++ src/components/form/Question.tsx
@@ -18,27 +18,28 @@ function Question({ title, choice, handleSelect, name }: Props) {
 
       {/* ตัวเลือก */}
       <div className="space-y-2">
-        {choice && choice.map((option) => (
-          <div
-            key={option}
-            className="flex items-center ps-4 border border-slate-300 bg-slate-50 rounded-xl"
-            onClick={() => handleSelect(option)}
-          >
-            <input
-              type="radio"
-              id={option}
-              name={name}
-              value={option}
-              className="w-4 h-4 text-blue-600"
-            />
-            <label
-              htmlFor={option}
-              className="w-full py-4 ms-2 text-sm font-medium"
+        {choice &&
+          choice.map((option) => (
+            <div
+              key={option}
+              className="flex items-center ps-4 border border-slate-300 bg-slate-50 rounded-xl"
+              onClick={() => handleSelect(option)}
             >
-              {option}
-            </label>
-          </div>
-        ))}
+              <input
+                type="radio"
+                id={option}
+                name={name}
+                value={option}
+                className="w-4 h-4 text-blue-600"
+              />
+              <label
+                htmlFor={option}
+                className="w-full py-4 ms-2 text-sm font-medium"
+              >
+                {option}
+              </label>
+            </div>
+          ))}
       </div>
     </>
   );
